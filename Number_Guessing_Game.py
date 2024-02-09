342478895b28b5703824fb091a49a1bf0bfb1895
Md Toriqul Islam <137867238+TheToriqul@users.noreply.github.com>
2024-02-09 22:27:06 +0800

    write the code

--- Number_Guessing_Game.py
+++ Number_Guessing_Game.py
@@ -1,1 +1,37 @@
-print("Welcome Onboard")
+import random
+
+print("Welcome to the number guessing game!")
+
+max_range = input("Type a maximum number of your guess: ")
+
+if max_range.isdigit():
+    max_range = int(max_range)
+    if max_range <= 0:
+        print("Please type a number less than 0 next time")
+        quit()
+else:
+    print("Please type a nice number next time")
+    quit()
+
+
+random_number = random.randint(0, max_range)
+
+
+guess_score = 0
+while True:
+    guess_score += 1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please type a number next time!")
+        continue
+    if user_guess == random_number:
+        print("You got the correct number :)")
+        break
+    elif user_guess > random_number:
+        print("You were above the number!")
+    else:
+        print("You were below the number!")
+
+print("You got it in", guess_score, "guesses. Congratulation!")
